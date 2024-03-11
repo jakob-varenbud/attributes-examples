b052d64cea549eb357a743d90021a0eb7c7dabd9
Jakob Varenbud <103386301+jakob-varenbud@users.noreply.github.com>
2024-03-11 10:38:33 +0100

    create new folder structure for different landing pages

---
+++ usa/cms/populate-external-data/utils/apiUtils.ts
@@ -0,0 +1,58 @@
+import type { Offer } from '../types/types';
+
+/**
+ * Asynchronously retrieves offers from the recruitee API.
+ * This function makes a GET request to the specified URL and
+ * parses the response as JSON, extracting the offers array.
+ *
+ * @returns A promise that resolves with an array of Offer objects,
+ *          or an empty array if the request fails.
+ */
+
+export async function fetchOffers(): Promise<Offer[]> {
+  try {
+    const response = await fetch('https://drsgroup.recruitee.com/api/offers/');
+    const data = await response.json();
+    let offers: Offer[] = data.offers;
+
+    // Exclude offers from "Vereinigte Staaten von Amerika"
+    offers = offers.filter((offer) => offer.country !== 'Vereinigte Staaten von Amerika');
+    offers = offers.filter((offer) => offer.country !== 'United States');
+
+    console.log(offers);
+    return offers;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+/**
+ * Collects unique tags and cities from an array of offers.
+ * This function iterates through each offer, adding each tag and city
+ * to a Set to ensure that only unique values are collected. It returns
+ * an object containing two arrays: one for tags and one for cities.
+ *
+ * @param offers An array of Offer objects to be processed.
+ * @returns An object containing arrays of unique tags and unique cities.
+ */
+
+export function collectTagsAndCitiesAndDepartments(offers: Offer[]) {
+  const tagsSet = new Set<string>();
+  const citiesSet = new Set<string>();
+  const departmentsSet = new Set<string>();
+
+  offers.forEach((offer) => {
+    if (offer.tags) {
+      offer.tags.forEach((tag) => tagsSet.add(tag));
+    }
+    if (offer.city) {
+      citiesSet.add(offer.city);
+    }
+    if (offer.department) {
+      departmentsSet.add(offer.department);
+    }
+  });
+
+  return { tags: [...tagsSet], cities: [...citiesSet], departments: [...departmentsSet] };
+}
